1b63b6b3db345d0eaa3cd4c18f93ca21e137b105
yoe7501 <yoel7501@gmail.com>
2024-10-23 14:59:54 -0400

    more errors :(

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import ContactForm from './component/ContactForm'
 import './styles/styles.css'
 import './globals.css'
-import Test from './component/test'
 
 export default function Home() {
   return (
