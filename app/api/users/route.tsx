b78761c668567aae14b686eaefdbadf7750384f5
yoe7501 <yoel7501@gmail.com>
2024-10-23 14:45:56 -0400

    initial commit

---
+++ app/api/users/route.tsx
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Submission {
+    firstName: string;
+    lastName: string;
+    email: string;
+    query: 'general' | 'support'; // Based on your enum
+    message?: string; // Optional field
+    consent: boolean; // Assuming consent is a boolean
+  }
+
+let submission: Submission[] = [];
+
+export async function GET(request: NextRequest) {
+    return NextResponse.json(submission);
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    submission.push(body);
+    return NextResponse.json(body);
+}
--- app/component/ContactForm.tsx
+++ app/component/ContactForm.tsx
@@ -1,29 +1,39 @@
 'use client'
-import React from "react";
+import React, { useState } from "react";
 import { SubmitHandler, useForm,  } from "react-hook-form";
 import {z} from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod'
+import axios from "axios";
+import { useRouter } from "next/navigation";
 enum QueryEnum {
   general = "general",
   support = 'support'
 }
 const schema = z.object({
   firstName: z.string({message: 'First Name is Required'}).min(4, {message: "Must be atleast 4 letters"}),
-  lastName: z.string().min(4),
-  email: z.string().min(10),
-  query: z.enum([QueryEnum.general, QueryEnum.support]),
-  message: z.string().optional(),
+  lastName: z.string({message: 'Last Name is Required'}).min(4, {message: 'Must be atleast 4 letters'}),
+  email: z.string().email({message: 'must be a valid email'}),
+  query: z.enum([QueryEnum.general, QueryEnum.support], {message: 'Required'}),
+  message: z.string().min(1, {message: 'message is required'}),
   consent: z.boolean().optional()
 })
 type Input = z.infer<typeof schema>;
 
+
+
 const ContactForm = () => {
-  const { register, handleSubmit, formState: {errors} } = useForm<Input>({
+  const router = useRouter();
+  const { register, handleSubmit, formState: {errors}, watch } = useForm<Input>({
     resolver: zodResolver(schema)
   });
-  const onSubmit: SubmitHandler<Input> = data => {
-    console.log(data); // Handle form submission
+  const onSubmit: SubmitHandler<Input> = async (data) => {
+    console.log(data.consent); // Handle form submission
+    await axios.post('../api/users', data);
+    router.push('../api/users');
   };
+
+  const isActive = watch('consent', false);
+  
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col bg-white justify-between p-10 rounded-lg min-h-[530px]">
@@ -38,13 +48,15 @@ const ContactForm = () => {
         </div>
         <div className="flex flex-col">
           <label htmlFor="lastName">Last Name</label>
-          <input id="lastName" {...register('lastName')} className="text" type="text"  />
+          <input id="lastName" {...register('lastName')} className={`text ${errors.lastName ? 'ring-red': 'ring-grey500'}`} type="text"  />
+          {errors.lastName && <p className="text-red">{errors.lastName.message}</p>}
         </div>
       </div>
       
       <div className="flex flex-col">
         <label htmlFor="email">Email</label>
-        <input id="email" {...register('email',{required: true, maxLength: 20})}  className="text" type="email"  />
+        <input id="email" {...register('email',{required: true, maxLength: 20})}  className={`text ${errors.email? 'ring-red': 'ring-grey500'}`} type="text"  />
+        {errors.email && <p className='text-red'>{errors.email.message}</p>}
       </div>
       
       <div>
@@ -73,19 +85,21 @@ const ContactForm = () => {
             <label htmlFor="support">Support Request</label>
           </div>
         </div>
+        {errors.query && <p className="text-red" >{errors.query.message}</p>}
       </div>
       
       <div className="flex flex-col">
         <label htmlFor="message">Message</label>
-        <textarea id="message" {...register('message')} className="text"  />
+        <textarea id="message" {...register('message')} className={`text ${errors.message? 'ring-red': 'ring-grey500'}`}  />
+        {errors.message && <p className="text-red">{errors.message.message}</p>}
       </div>
       
       <div>
-        <input type="checkbox" id="consent" {...register('consent')} className="mr-3"  />
+        <input type="checkbox" checked={isActive} id="consent" {...register('consent')} className={`mr-3 `}  />
         <label htmlFor="consent">I consent to being contacted by the team</label>
       </div>
       
-      <button className="bg-green600 hover:bg-grey-500 rounded-md h-10 text-white">Submit</button>
+      <button disabled={!isActive} className="bg-green600 hover:bg-grey-500 rounded-md h-10 text-white">Submit</button>
     </form>
   );
 };
