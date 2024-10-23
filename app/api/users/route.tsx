3b90798c4a6308b7bed99950d64d466fcf3329a4
yoe7501 <yoel7501@gmail.com>
2024-10-23 15:02:48 -0400

    idk

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -11,7 +11,7 @@ interface Submission {
 
 const submission: Submission[] = [];
 
-export async function GET(request: NextRequest) {
+export async function GET() {
     return NextResponse.json(submission);
 }
 
